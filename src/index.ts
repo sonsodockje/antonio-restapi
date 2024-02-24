fe3de97e8b479fd11c2d485cb4471ac91e06d663
sonsodockje <hanaroo0115@naver.com>
2024-02-24 17:41:36 +0900

    db

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import express from "express";
 import http from "http";
+import mongoose, { Promise } from "mongoose";
 
 const app = express();
 app.use(cors({ credentials: true }));
@@ -17,3 +18,11 @@ const server = http.createServer(app);
 server.listen(8080, () => {
   console.log("서버온 http://localhost:8080/");
 });
+
+const MONGO_URL =
+  "mongodb+srv://lit0goguma:uuhAgHnkcr5Ss2V3@cluster0.bnm6uj0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+
+mongoose.connection.on("error", (error: Error) => console.log(error));
