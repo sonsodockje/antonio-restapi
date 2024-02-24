c9ee6835a59784cae2e5f1d5a5061111ebce0039
sonsodockje <hanaroo0115@naver.com>
2024-02-24 17:20:59 +0900

    서버 세팅 완료

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log("hello");
+import bodyParser from "body-parser";
+import compression from "compression";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express from "express";
+import http from "http";
+
+const app = express();
+app.use(cors({ credentials: true }));
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+server.listen(8080, () => {
+  console.log("서버온 http://localhost:8080/");
+});
